a36d0b4192cdc723bdf592999e230066d703665b
William Jackson <565174+williamjacksn@users.noreply.github.com>
2022-06-02 16:09:46 -0500

    Add dependencies to build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,9 @@
 set -e
 
 # install dependencies
-# apk add --no-cache gcc musl-dev
+apk add --no-cache acl-dev gcc linux-headers lz4-dev musl-dev openssl-dev python3-dev xxhash-deve zstd-dev
+
+pip install pkgconfig
 
 # build
 pip wheel --no-deps --requirement requirements.txt
